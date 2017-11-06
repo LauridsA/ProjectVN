f4bea399c14d50834589d63e8120c7067d29edea
LauridsA <lauridsandersen2013@gmail.com>
2017-11-06 10:35:39 +0700

    blablabla

--- FrontEnd/src/App.jsx
+++ FrontEnd/src/App.jsx
@@ -1,7 +1,4 @@
 import React from 'react'
-import Hello from './Hello.jsx'
-import Greeting from './Greeting.jsx'
-import Bye from './Bye.jsx'
 import StudentForm from './StudentForm.jsx'
 import Product from './Product.jsx'
 
@@ -9,7 +6,7 @@ export default class App extends React.Component{
     render(){
         return(
             <div>
-            // do nothing ...
+            blah.
             </div>
             
             )
